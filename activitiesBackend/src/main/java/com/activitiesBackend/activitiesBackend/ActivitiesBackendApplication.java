3a4ba9b8c7c00297e11424af04ff3542109bb2d8
Madhura Ashture <90354562+madhuraashture@users.noreply.github.com>
2024-08-04 17:21:21 +0530

    Add files via upload
    Spring folder structure

---
+++ activitiesBackend/src/main/java/com/activitiesBackend/activitiesBackend/ActivitiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.activitiesBackend.activitiesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiesBackendApplication.class, args);
+	}
+
+}
---
+++ activitiesBackend/src/test/java/com/activitiesBackend/activitiesBackend/ActivitiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activitiesBackend.activitiesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
