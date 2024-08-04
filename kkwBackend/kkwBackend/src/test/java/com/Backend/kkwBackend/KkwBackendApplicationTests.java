8e8e8a95fb343333fbf49ae894d8ca61120a5886
Madhura Ashture <90354562+madhuraashture@users.noreply.github.com>
2024-08-04 17:07:17 +0530

    Add files via upload
    Backend Folder Structure

---
+++ kkwBackend/kkwBackend/src/test/java/com/Backend/kkwBackend/KkwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.kkwBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
