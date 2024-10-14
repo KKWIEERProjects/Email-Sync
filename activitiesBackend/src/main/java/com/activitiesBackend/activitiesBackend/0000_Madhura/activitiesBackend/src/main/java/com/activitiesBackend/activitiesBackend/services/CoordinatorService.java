9af15618aafa0305c8f919a50e65080808e6d003
Madhura Ashture <90354562+madhuraashture@users.noreply.github.com>
2024-10-15 00:35:08 +0530

    Coordinator Service

---
+++ activitiesBackend/src/main/java/com/activitiesBackend/activitiesBackend/0000_Madhura/activitiesBackend/src/main/java/com/activitiesBackend/activitiesBackend/services/CoordinatorService.java
@@ -0,0 +1,16 @@
+package com.activitiesBackend.activitiesBackend.services;
+
+import java.util.List;
+
+import com.activitiesBackend.activitiesBackend.Entities.hod_ntt;
+
+public class CoordinatorService 
+{
+
+	public List<hod_ntt> fetchHodList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
