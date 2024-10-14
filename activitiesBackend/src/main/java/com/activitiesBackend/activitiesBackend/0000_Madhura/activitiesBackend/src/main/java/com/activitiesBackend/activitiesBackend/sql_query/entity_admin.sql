eb203f69041706c766b0a750d433423fd677515a
Madhura Ashture <90354562+madhuraashture@users.noreply.github.com>
2024-10-15 00:50:45 +0530

    admin entity sql file

---
+++ activitiesBackend/src/main/java/com/activitiesBackend/activitiesBackend/0000_Madhura/activitiesBackend/src/main/java/com/activitiesBackend/activitiesBackend/sql_query/entity_admin.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `activitiesdb`.`hod` (
+  `hod_id` INT NOT NULL,
+  `hod_name` VARCHAR(45) NULL,
+  `department` VARCHAR(45) NULL,
+  `phone` VARCHAR(45) NULL,
+  `address` VARCHAR(45) NULL,
+  `bdate` VARCHAR(45) NULL);
