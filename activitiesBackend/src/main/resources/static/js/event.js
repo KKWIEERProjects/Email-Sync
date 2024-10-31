8f39a809c03d520b8fadfa301cdcf10b52b80d97
sumedhahire <ahiresumedhbusiness@gmail.com>
2024-10-31 10:09:33 +0530

    login toast corrected

--- activitiesBackend/src/main/resources/static/js/event.js
+++ activitiesBackend/src/main/resources/static/js/event.js
@@ -103,25 +103,3 @@ document.getElementById("demo").innerHTML = formattedDate;
 document.getElementById("demo1").innerHTML = formattedDate;
 
 
-
-
-// // Function to show the popup
-// function showPopup() {
-//     document.getElementById('popupForm').style.display = 'block';
-//     document.getElementById('overlay').style.display = 'block';
-// }
-
-// // Function to hide the popup
-// function closePopup() {
-//     document.getElementById('popupForm').style.display = 'none';
-//     document.getElementById('overlay').style.display = 'none';
-// }
-
-// // Event listener for the Create Event button
-// document.querySelector('.create-event').addEventListener('click', showPopup);
-
-// // Event listener to close the popup when clicking outside the form
-// document.getElementById('overlay').addEventListener('click', closePopup);
-
-// // Event listener for the Close button inside the popup form
-// document.getElementById('closePopup').addEventListener('click', closePopup);
